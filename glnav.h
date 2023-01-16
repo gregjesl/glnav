ed1faf3a87657b314dfd404f044a141dc057ba00
Gregory Eslinger <gregjesl@gmail.com>
2023-01-16 16:12:30 -0700

    Removed pin

--- glnav.h
+++ glnav.h
@@ -2,9 +2,7 @@
 #define GLNAV_H
 
 #include "inc/glnav_point.h"
-#include "inc/glnav_pin.h"
 #include "inc/glnav_path.h"
-#include "inc/glnav_fence.h"
 #include "inc/glnav_corner.h"
 #include "inc/glnav_obstacle.h"
 
