1debd545ac76c3c785e796633cd9644daa86aace
Gregory Eslinger <gregjesl@gmail.com>
2023-01-21 03:06:58 -0700

    Network completed!

--- inc/glnav_network.h
+++ inc/glnav_network.h
@@ -3,89 +3,150 @@
 
 #include "glnav_point.h"
 #include "glnav_edge.h"
-#include "glnav_obstacle_interface.h"
 #include <assert.h>
 #include <map>
 
 namespace glnav
 {
     template<typename T>
-    class network
+    class neighbor_map : private std::map<const point<T> * const, double>
     {
     public:
-        network(const obstacle_group<T> * obstacles)
-            : __obstacles(obstacles)
+        neighbor_map()
+            : std::map<const point<T> * const, double>()
+        { }
+
+        void update(const point<T> * const neighbor, const double cost)
         {
-            if(obstacles == NULL) throw std::invalid_argument("Null pointer");
-
-            this->__nodes = obstacles->corners();
-
-            for(typename point_group<T>::const_iterator it1 = this->__nodes.begin();
-                it1 != this->__nodes.end();
-                ++it1)
+            typename std::map<const point<T> * const, double>::iterator it = this->find(neighbor);
+            if(it == this->end())
             {
-                const point<T> * point1 = &(*it1);
-
-                // TODO: Optimize
-                for(typename point_group<T>::const_iterator it2 = it1;
-                    it2 != this->__nodes.end();
-                    ++it2)
-                {
-                    const point<T> * point2 = &(*it2);
-
-                    // Check for same point
-                    if(point1 == point2) continue;
-
-                    // Check for existing edge
-                    edge<T> test(point1, point2);
-                    if(this->__edges.find(test) != this->__edges.end()) 
-                    {
-                        continue;
-                    }
-                    
-                    const path<T> test_path = test.as_path();
-
-                    // Check for path
-                    if(!obstacles->obstructs(test_path))
-                    {
-                        this->__edges.insert(
-                            std::pair<edge<T> , double>(
-                                test, 
-                                test_path.length()
-                            )
-                        );
-                    }
-                }
+                this->insert(
+                    std::pair<const point<T> * const, double>(neighbor, cost)
+                );
+            }
+            else
+            {
+                it->second = cost;
             }
         }
 
-        size_t num_nodes() const { return this->__nodes.size(); }
-        size_t num_edges() const { return this->__edges.size(); }
-
-        /*
-        void navigate(const point<T> &from, const point<T> &to)
+        double cost(const point<T> * const to) const
         {
-            // Check for trivial case
-            if(this->__corners.size() == 0)
-            {
-                // TODO: Return path
-                return;
-            }
-
-            // typedef std::map<point<T> *>, double> cost_map_t;
-            // cost_map_t cost_map;
-
-            // Seed the cost map
+            typename std::map<const point<T> * const, double>::const_iterator it = this->find(to);
+            return it != this->end() ? it->second : std::numeric_limits<double>::infinity();
         }
-        */
-    private:
-        const obstacle_group<T> *__obstacles;
 
-        point_group<T> __nodes;
-
-        typedef std::map<const edge<T> , double> edge_map_t;
-        edge_map_t __edges;
+        std::vector<std::pair<point<T>, double> > neighbors() const
+        {
+            std::vector<std::pair<point<T>, double> > result;
+            typename std::map<const point<T> * const, double>::const_iterator it;
+            for(it = this->begin(); it != this->end(); ++it)
+            {
+                result.push_back(
+                    std::pair<point<T>, double>(*it->first, it->second)
+                    );
+            }
+            return result;
+        }
     };
+
+    #define network_value_t std::pair<neighbor_map<T>, Q>
+    #define network_base_t std::map<const point<T>,  network_value_t>
+
+    template<typename T, typename Q>
+    class network : private network_base_t
+    {
+    public:
+        network()
+            : network_base_t()
+        { }
+        
+        void add(const path<T> &seed, const double cost, Q metadata)
+        {
+            this->__add(seed.start, seed.end, cost, metadata);
+            this->__add(seed.end, seed.start, cost, metadata);
+        }
+
+        bool contains(const point<T> &node) const
+        {
+            return this->find(node) != this->end();
+        }
+
+        using network_base_t::size;
+
+        double cost(const point<T> &from, const point<T> &to) const
+        {
+            typename network_base_t::const_iterator start_it = this->find(from);
+            if(start_it == this->end()) return std::numeric_limits<double>::infinity();
+
+            typename network_base_t::const_iterator end_it = this->find(to);
+            if(end_it == this->end()) return std::numeric_limits<double>::infinity();
+            const point<T> * end_ptr = &end_it->first;
+
+            return start_it->second.first.cost(end_ptr);
+        }
+
+        std::vector<std::pair<point<T>, double> > neighbors(const point<T> &node) const
+        {
+            std::vector<std::pair<point<T>, double> > result;
+            typename network_base_t::const_iterator start_it = this->find(node);
+            if(start_it == this->end()) return result;
+            return start_it->second.first.neighbors();
+        }
+
+        Q* metadata(const point<T> &node)
+        {
+            typename network_base_t::iterator start_it = this->find(node);
+            if(start_it == this->end()) return nullptr;
+            return &start_it->second.second;
+        }
+
+    private:
+        void __add(const point<T> &start, const point<T> &end, const double cost, Q metadata)
+        {
+            typename network_base_t::iterator start_it = this->find(start);
+            while(start_it == this->end())
+            {
+                this->insert(
+                        std::pair<
+                            point<T>,
+                            network_value_t
+                        >
+                        (
+                            start,
+                            network_value_t()
+                        )
+                    );
+                start_it = this->find(start);
+            }
+
+            typename network_base_t::iterator end_it = this->find(end);
+            while(end_it == this->end())
+            {
+                this->insert(
+                        std::pair<
+                            point<T>,
+                            network_value_t
+                        >
+                        (
+                            end,
+                            network_value_t()
+                        )
+                    );
+                end_it = this->find(end);
+            }
+            const point<T> *end_ptr = &end_it->first;
+
+            assert(start_it != this->end());
+            assert(end_ptr != nullptr);
+            start_it->second.first.update(end_ptr, cost);
+            start_it->second.second = metadata;
+        }
+    };
+
+    #undef network_base_t
+    #undef network_value_t
 }
 
 #endif
--- test/network.cpp
+++ test/network.cpp
@@ -3,23 +3,35 @@
 
 int main(void)
 {
-    // Create two obstacles
-    glnav::obstacle<int> test_obs1 = glnav::obstacle<int>::square(
-        glnav::point<int>(2, 2), 2
-    );
-
-    glnav::obstacle<int> test_obs2 = glnav::obstacle<int>::square(
-        glnav::point<int>(-2, -2), 2
-    );
-
-    // Create the group
-    glnav::obstacle_group<int> test;
-    test.add(&test_obs1);
-    test.add(&test_obs2);
-    TEST_EQUAL(test.size(), 2);
-
     // Create the network
-    glnav::network<int> test_network(&test);
-    TEST_EQUAL(test_network.num_nodes(), 8);
-    TEST_EQUAL(test_network.num_edges(), 8 + 9);
+    glnav::network<int, double> test;
+    TEST_EQUAL(test.size(), 0);
+
+    // Add a path
+    const glnav::point<int> start(1, 2);
+    const glnav::point<int> end(3, 4);
+    const glnav::path<int> test_path(start, end);
+    test.add(test_path, test_path.length(), 1.0);
+    TEST_EQUAL(test.size(), 2);
+    TEST_TRUE(test.contains(start));
+    TEST_TRUE(test.contains(end));
+    TEST_FALSE(test.contains(glnav::point<int>(5, 6)));
+    TEST_TRUE(test.cost(start, end) > 2.0);
+    TEST_TRUE(test.cost(start, end) < 4.0);
+    TEST_EQUAL(test.neighbors(start).size(), 1);
+    TEST_TRUE(test.neighbors(start).at(0).first == end);
+    TEST_TRUE(test.neighbors(end).at(0).first == start);
+    TEST_NOT_NULL(test.metadata(start));
+    TEST_EQUAL(*test.metadata(start), 1.0);
+    TEST_NULL(test.metadata(glnav::point<int>(5, 6)));
+    *test.metadata(start) = 2;
+    TEST_EQUAL(*test.metadata(start), 2.0);
+
+    // Attempt to add the same path
+    test.add(test_path, test_path.length(), 1.0);
+    TEST_EQUAL(test.size(), 2);
+    TEST_TRUE(test.contains(start));
+    TEST_TRUE(test.contains(end));
+    TEST_TRUE(test.cost(start, end) > 2.0);
+    TEST_TRUE(test.cost(start, end) < 4.0);
 }
